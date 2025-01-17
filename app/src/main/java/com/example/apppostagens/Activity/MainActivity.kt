794b3bdaa5ba002f66ae7cb9dab63f4abdb2dae5
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2025-01-17 02:45:09 -0300

    Transferindo as funções do feed para o FeedFragment

--- app/src/main/java/com/example/apppostagens/Activity/MainActivity.kt
+++ app/src/main/java/com/example/apppostagens/Activity/MainActivity.kt
@@ -1,47 +1,25 @@
 package com.example.apppostagens.Activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.apppostagens.Adapter.PostAdapter
-import com.example.apppostagens.Model.Post
-import com.example.apppostagens.Model.User
+import com.example.apppostagens.Fragments.FeedFragment
 import com.example.apppostagens.R
+import com.example.apppostagens.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var list: RecyclerView
-    private var listPosts: MutableList<Post> = mutableListOf()
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.list)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        createPost()
-        list = findViewById(R.id.list)
-        val adapter = PostAdapter(listPosts)
-        val layoutManager = LinearLayoutManager(applicationContext)
 
-        list.layoutManager = layoutManager
-        list.setHasFixedSize(true)
-        list.adapter = adapter
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerMain, FeedFragment())
+            .commit()
 
     }
-    fun createPost(){
-        // Criando o usuário teste
-        val user1 = User("Amelia", R.drawable.black)
-        //Criando os posts testes
-        val post1 = Post("The beautiful sunset!", R.drawable.sunset, "01-02-2025", user1, user1)
-        val post2 = Post("My sweet cat <3", R.drawable.cat, "01-08-2025", user1, user1)
-        listPosts.add(post1)
-        listPosts.add(post2)
-    }
+
 }
