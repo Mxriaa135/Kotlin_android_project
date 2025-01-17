71961e2cd15c1c15247bd638d9a6db604baa7be3
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2025-01-17 02:47:21 -0300

    Inserindo o binding

--- app/src/main/java/com/example/apppostagens/Activity/CommentActivity.kt
+++ app/src/main/java/com/example/apppostagens/Activity/CommentActivity.kt
@@ -10,19 +10,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.apppostagens.Adapter.CommentAdapter
 import com.example.apppostagens.Fragments.CommentFragment
 import com.example.apppostagens.R
+import com.example.apppostagens.databinding.ActivityCommentBinding
+import com.example.apppostagens.databinding.ActivityMainBinding
 import org.w3c.dom.Comment
 
 class CommentActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityCommentBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_comment)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityCommentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val postId = intent.getStringExtra("POST_ID")
 
