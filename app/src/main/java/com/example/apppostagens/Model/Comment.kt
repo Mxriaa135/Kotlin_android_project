9c20975ae1615c83abd3a61563a9c9d68580549d
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2025-01-20 22:23:57 -0300

    Criando getters e setters

--- app/src/main/java/com/example/apppostagens/Model/Comment.kt
+++ app/src/main/java/com/example/apppostagens/Model/Comment.kt
@@ -1,5 +1,34 @@
 package com.example.apppostagens.Model
 
-class Comment(var user: User, var name: User, var userImage: User, var texto: String) {
-    var post: Post? = null
+class Comment(
+    private var post: Post,
+    private var user: User,
+    private var name: User,
+    private var userImage: User,
+    private var text: String)
+{
+
+    fun getName(): User {
+        return name
+    }
+
+    fun setName(name: User) {
+        this.name = name
+    }
+
+    fun getUserImage(): User {
+        return userImage
+    }
+
+    fun setUserImage(userImage: User) {
+        this.userImage = userImage
+    }
+
+    fun getText(): String{
+        return text
+    }
+
+    fun setText(text: String){
+        this.text = text
+    }
 }
