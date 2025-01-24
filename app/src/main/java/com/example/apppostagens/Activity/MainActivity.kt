c06456ad1b2bade9eab0ac5dfbba31dfe48c9601
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2025-01-23 23:48:56 -0300

    Alteração e inicialização dos fragments com o bottomNavigation

--- app/src/main/java/com/example/apppostagens/Activity/MainActivity.kt
+++ app/src/main/java/com/example/apppostagens/Activity/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.apppostagens.Activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.apppostagens.Fragments.FeedFragment
 import com.example.apppostagens.R
 import com.example.apppostagens.databinding.ActivityMainBinding
+import com.example.apppostagens.databinding.NavigationBottomBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +21,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainerMain, FeedFragment())
-            .commit()
+        val navController = findNavController(R.id.nav_host_fragment_container_Main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bnve)
+        bottomNavigationView.setupWithNavController(navController)
 
     }
 
