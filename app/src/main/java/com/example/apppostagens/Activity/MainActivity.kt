d819893ac9e29fc20a680c539145d92887abedbe
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2025-02-19 23:45:52 -0300

    Criando a function createPost teste

--- app/src/main/java/com/example/apppostagens/Activity/MainActivity.kt
+++ app/src/main/java/com/example/apppostagens/Activity/MainActivity.kt
@@ -5,18 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.apppostagens.Fragments.FeedFragment
+import com.example.apppostagens.Model.Post
+import com.example.apppostagens.Model.User
 import com.example.apppostagens.R
 import com.example.apppostagens.databinding.ActivityMainBinding
 import com.example.apppostagens.databinding.NavigationBottomBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private var reference: DatabaseReference = FirebaseDatabase.getInstance().getReference()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        createPost()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -26,5 +33,15 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView.setupWithNavController(navController)
 
     }
+    fun createPost(){
+        // Criando o usuário teste
+        var user1 = User("Amelia", R.drawable.black)
+        reference.child("User").push().setValue(user1)
 
+        //Criando os posts testes
+        var post1 = Post(1,"The beautiful sunset!", R.drawable.sunset, "01-02-2025", user1, user1)
+        var post2 = Post(2,"My sweet cat <3", R.drawable.cat, "01-08-2025", user1, user1)
+        reference.child("Post").push().setValue(post1)
+        reference.child("Post").push().setValue(post2)
+    }
 }
