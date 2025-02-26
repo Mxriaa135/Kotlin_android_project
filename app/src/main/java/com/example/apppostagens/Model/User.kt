3090762448fc6e81aa76251a9885975865f00d12
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2025-02-26 15:01:05 -0300

    Adicionado construtor vazio na classe User para compatibilidade com Firebase

--- app/src/main/java/com/example/apppostagens/Model/User.kt
+++ app/src/main/java/com/example/apppostagens/Model/User.kt
@@ -1,17 +1,17 @@
 package com.example.apppostagens.Model
 
-class User {
-    var name: String
-    var userImage: Int
+class User() {
+    var name: String = ""
+    var userImage: String = ""
     var email: String? = null
     var telefone: String? = null
 
-    constructor(name: String, userImage: Int) {
+    constructor(name: String, userImage: String) : this(){
         this.name = name
         this.userImage = userImage
     }
 
-    constructor(name: String, image: Int, email: String?, telefone: String?) {
+    constructor(name: String, image: String, email: String?, telefone: String?) : this(){
         this.name = name
         this.userImage = image
         this.email = email
