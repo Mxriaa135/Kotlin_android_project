cf5e6d56c1f91df04666578324ac2ffdb1b5c549
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2025-03-28 22:28:03 -0300

    Implementando a toolbar e criando a function signOut no ProfileFragment

--- app/src/main/java/com/example/apppostagens/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/apppostagens/Fragments/ProfileFragment.kt
@@ -1,14 +1,27 @@
 package com.example.apppostagens.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import com.bumptech.glide.manager.Lifecycle
+import com.example.apppostagens.Activity.LoginActivity
 import com.example.apppostagens.R
+import com.example.apppostagens.Utils.FirebaseConfiguration
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var authenticator : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +31,44 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        var view : View = inflater.inflate(R.layout.fragment_profile, container, false)
+        val view : View = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        //toolbar
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar_profile)
+        toolbar.setTitle("")
+        (activity as? AppCompatActivity)?.setSupportActionBar(toolbar)
+
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_profile, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.menu_sair -> {
+                        signOut()
+                        startActivity(Intent(context, LoginActivity::class.java))
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner)
 
         return view
+    }
+    private fun signOut(){
+        authenticator = FirebaseConfiguration.getFirebaseAuthReference()
+
+        try {
+            authenticator.signOut()
+        } catch (e :Exception){
+            e.printStackTrace()
+        }
     }
 
 }
