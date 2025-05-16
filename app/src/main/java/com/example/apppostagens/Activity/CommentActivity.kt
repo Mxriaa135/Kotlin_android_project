486af6182ac96bd8f9e4e30187cc1bd141504ba3
Mxriaa135 <mariaclaracorreamoraes6@gmail.com>
2025-05-16 13:50:54 -0300

    Configurando o Comment Adapter e criando a function loadData()

--- app/src/main/java/com/example/apppostagens/Activity/CommentActivity.kt
+++ app/src/main/java/com/example/apppostagens/Activity/CommentActivity.kt
@@ -1,47 +1,90 @@
 package com.example.apppostagens.Activity
 
 import android.os.Bundle
-import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.apppostagens.Adapter.CommentAdapter
-import com.example.apppostagens.Fragments.CommentFragment
-import com.example.apppostagens.R
+import com.example.apppostagens.Model.Comment
+import com.example.apppostagens.Utils.FirebaseConfiguration
+import com.example.apppostagens.Utils.UserFirebase
 import com.example.apppostagens.databinding.ActivityCommentBinding
-import com.example.apppostagens.databinding.ActivityMainBinding
-import org.w3c.dom.Comment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 
 class CommentActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityCommentBinding
+    private lateinit var eventListener: ValueEventListener
+    private lateinit var comment: Comment
+    private lateinit var postId: String
+    private lateinit var idUser: String
+    private var listComments: MutableList<Comment> = mutableListOf()
+    private lateinit var commentRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityCommentBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initializeComponents()
 
-        val postId = intent.getStringExtra("POST_ID")
+        val list = binding.listComment
+        var layoutManager = LinearLayoutManager(this)
+        list.layoutManager = layoutManager
+        list.setHasFixedSize(true)
 
-        val fragment = CommentFragment().apply {
-            arguments = Bundle().apply {
-                putString("POST_ID", postId)
+        var adapter = CommentAdapter(listComments)
+        list.adapter = adapter
+        loadData(adapter)
+
+        binding.imageArrow.setOnClickListener {
+            comment.add()
+            comment.setText(binding.editTextComment.text.toString())
+            comment.setIdPost(postId)
+            comment.setIdUser(idUser)
+            if(binding.editTextComment.text.isNotBlank()){
+                comment.save()
+                binding.editTextComment.text.clear()
             }
+            adapter.notifyDataSetChanged()
         }
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainerComment, fragment)
-            .commit()
-
-        val bttnBack = findViewById<ImageView>(R.id.imageArrowBack)
-        bttnBack.setOnClickListener {
+        binding.imageArrowBack.setOnClickListener {
             finish()
         }
 
+    }
+
+    private fun loadData(adapter: CommentAdapter){
+        eventListener = commentRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(ds: DataSnapshot) {
+                listComments.clear()
+                for(snapshot in ds.children){
+                    val comment = snapshot.getValue(Comment::class.java)
+                    comment?.let{
+                        listComments.add(comment)
+                    }
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    private fun initializeComponents(){
+        comment = Comment()
+        idUser = UserFirebase.getCurrentUser()!!.uid
+        postId = intent.getStringExtra("POST_ID")!!
+        commentRef = FirebaseConfiguration.getFirebaseReference().child("Comments").child(postId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        commentRef.removeEventListener(eventListener)
     }
 
 }
